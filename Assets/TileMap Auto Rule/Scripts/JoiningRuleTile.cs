49b8bb73562d4f79f5764485c38e6c0dbde92bd9
Jasper Honkasalo <japsu.honkasalo@gmail.com>
2021-08-31 17:51:55 +0300

    ti 31.08.2021 17.51.55,34: texture updates

--- Assets/TileMap Auto Rule/Scripts/JoiningRuleTile.cs
+++ Assets/TileMap Auto Rule/Scripts/JoiningRuleTile.cs
@@ -11,6 +11,7 @@ public class JoiningRuleTile : RuleTile<JoiningRuleTile.Neighbor>
     public class Neighbor : RuleTile.TilingRule.Neighbor
     {
         public const int ThisOrFriend = 1;
+        public const int Friend = 3;
     }
 
     public override bool RuleMatch(int neighbor, TileBase tile)
@@ -19,13 +20,15 @@ public class JoiningRuleTile : RuleTile<JoiningRuleTile.Neighbor>
         {
             case Neighbor.ThisOrFriend:
                 return tile == this || HasFriendTile(tile);
+            case Neighbor.Friend:
+                return HasFriendTile(tile);
             case TilingRuleOutput.Neighbor.NotThis:
                 return tile == null;
         }
         return true;
     }
 
-    bool HasFriendTile(TileBase tile)
+    private bool HasFriendTile(TileBase tile)
     {
         if (tile == null)
             return false;
@@ -33,9 +36,9 @@ public class JoiningRuleTile : RuleTile<JoiningRuleTile.Neighbor>
         if (friendTiles.Length < 1)
             return false;
 
-        for (int i = 0; i < friendTiles.Length; i++)
+        foreach (var t in friendTiles)
         {
-            if (friendTiles[i] == tile)
+            if (t == tile)
                 return true;
         }
         return false;
